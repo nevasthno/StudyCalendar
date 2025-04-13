5e222df219de261a6a17a21c85b383469acf86ce
VladyslavMoshniahul <163896225+VladyslavMoshniahul@users.noreply.github.com>
2025-04-13 17:14:07 +0300

    Update People.java

--- demo/src/main/java/com/example/demo/javaSrc/people/People.java
+++ demo/src/main/java/com/example/demo/javaSrc/people/People.java
@@ -1,40 +1,36 @@
 package com.example.demo.javaSrc.people;
 
-import java.util.Date;
-import java.io.*;
 
-public class Event {
-    enum EventType {
-        EXAM, TEST, SCHOOL_EVENT, PARENTS_MEETING, PERSONAL
+import java.sql.Date;
+
+public class People {
+    public enum Role { TEACHER, STUDENT, PARENT }
+    
+    private String firstName, lastName, aboutMe, email, password;
+    private Date dateOfBirth;
+    private Role role;
+
+    public People(String firstName, String lastName, String aboutMe, Date dateOfBirth, String email, String password, Role role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.aboutMe = aboutMe;
+        this.dateOfBirth = dateOfBirth;
+        this.email = email;
+        this.password = password;
+        this.role = role;
     }
 
-    String title, content, locationOrLink, link;
-    Task task;
-    int duration;
-    Date start;
-    File file;
-    EventType eventType;
-
-    public Event(String title, Date start, String content, String locationOrLink, Task task, int duration, String link, File file, EventType eventType) {
-        this.title = title;
-        this.start = start;
-        this.content = content;
-        this.locationOrLink = locationOrLink;
-        this.task = task;
-        this.duration = duration;
-        this.link = link;
-        this.file = file;
-        this.eventType = eventType;
-    }
-
-    public String getTitle() { return title; }
-    public Date getStart() { return start; }
-    public String getContent() { return content; }
-    public String getLocationOrLink() { return locationOrLink; }
-    public Task getTask() { return task; }
-    public int getDuration() { return duration; }
-    public String getLink() { return link; }
-    public File getFile() { return file; }
-    public EventType getEventType() { return eventType; }
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public String getEmail() { return email; }
+    public Role getRole() { return role; }
+   
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    public void setAboutMe(String aboutMe) { this.aboutMe = aboutMe; }
+    public void setDateOfBirth(Date dateOfBirth) { this.dateOfBirth = dateOfBirth; }
 }
+
+
+
 
