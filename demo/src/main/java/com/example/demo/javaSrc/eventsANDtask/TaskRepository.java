6db816881c81cb934083c3d70fee05dad2f76c20
VladyslavMoshniahul <163896225+VladyslavMoshniahul@users.noreply.github.com>
2025-04-21 16:55:17 +0300

    Update TaskRepository.java

--- demo/src/main/java/com/example/demo/javaSrc/eventsANDtask/TaskRepository.java
+++ demo/src/main/java/com/example/demo/javaSrc/eventsANDtask/TaskRepository.java
@@ -1,8 +1,21 @@
 package com.example.demo.javaSrc.eventsANDtask;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findByUserId(Long userId);
+
+    List<Task> findByUserIdAndDeadline(Long userId, Date date);
+
+    boolean TaskIsCompleted(Long userId, Long taskId);
+
+    void markTaskAsCompleted(Long userId, Long taskId);
+
 }
+
