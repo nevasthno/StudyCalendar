f7793bccc4406b28af4a830d1f5df8251a8093f0
VladyslavMoshniahul <163896225+VladyslavMoshniahul@users.noreply.github.com>
2025-05-14 16:18:54 +0300

    Add forgotten import

--- demo/src/main/java/com/example/demo/javaSrc/eventsANDtask/EventRepository.java
+++ demo/src/main/java/com/example/demo/javaSrc/eventsANDtask/EventRepository.java
@@ -1,5 +1,8 @@
 package com.example.demo.javaSrc.eventsANDtask;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
