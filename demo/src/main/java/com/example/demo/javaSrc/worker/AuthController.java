aa53321aa514ba38f0ac1527c76d3ffe7566385e
VladyslavMoshniahul <163896225+VladyslavMoshniahul@users.noreply.github.com>
2025-05-18 20:39:13 +0300

    Update AuthController.java

--- demo/src/main/java/com/example/demo/javaSrc/worker/AuthController.java
+++ demo/src/main/java/com/example/demo/javaSrc/worker/AuthController.java
@@ -57,15 +57,4 @@ public class AuthController {
     }
     
 }
-class AuthRequest {
-    private String email;
-    private String password;
-    private String role;
 
-    public String getEmail()    { return email; }
-    public String getPassword() { return password; }
-    public String getRole()     { return role; }
-    public void setEmail(String e)    { this.email = e; }
-    public void setPassword(String p) { this.password = p; }
-    public void setRole(String r)     { this.role = r; }
-}
